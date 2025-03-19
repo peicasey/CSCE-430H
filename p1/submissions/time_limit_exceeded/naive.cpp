7f1f470baca71e36ccdd9478ff11de9360ec7a14
peicasey <peicasey@gmail.com>
2025-03-19 02:24:33 -0500

    add comments to naive result

--- p1/submissions/time_limit_exceeded/naive.cpp
+++ p1/submissions/time_limit_exceeded/naive.cpp
@@ -12,13 +12,11 @@ int main() {
     vector<string> players(n);
     unordered_map<string, int> player_index;
 
-    // Reading players and creating a map from player name to their index
     for (int i = 0; i < n; ++i) {
         cin >> players[i];
         player_index[players[i]] = i;
     }
 
-    // Processing the operations
     for (int i = 0; i < k; ++i) {
         string player_x, player_y, operation;
         cin >> player_x >> operation >> player_y;
@@ -32,17 +30,20 @@ int main() {
             players.erase(players.begin() + x_idx);
             int insert_pos = (x_idx < y_idx) ? y_idx - 1 : y_idx;
             players.insert(players.begin() + insert_pos, temp);
-            // Update the player index map after the change
+            
+            // update the player index map after the change
             for (int j = 0; j < n; ++j) {
                 player_index[players[j]] = j;
             }
+
         } else if (operation == "<") {
             // player X < player Y -> place X immediately after Y
             string temp = players[x_idx];
             players.erase(players.begin() + x_idx);
             int insert_pos = (x_idx < y_idx) ? y_idx : y_idx + 1;
             players.insert(players.begin() + insert_pos, temp);
-            // Update the player index map after the change
+            
+            // update the player index map after the change
             for (int j = 0; j < n; ++j) {
                 player_index[players[j]] = j;
             }
